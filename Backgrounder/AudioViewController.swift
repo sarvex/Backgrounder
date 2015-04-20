1eb1f8c8da6d89b893fcc3c8e47e8df0ed42b31c
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2015-04-20 16:41:57 +0530

    Completed Music Player

--- Backgrounder/AudioViewController.swift
+++ Backgrounder/AudioViewController.swift
@@ -7,13 +7,60 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AudioViewController: UIViewController {
-  
-  @IBOutlet weak var songLabel: UILabel!
-  @IBOutlet weak var timeLabel: UILabel!
-  
-  @IBAction func playPauseAction(sender: UIButton) {
-  }
-}
 
+    var player:AVQueuePlayer!
+
+    @IBOutlet weak var songLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+
+    @IBAction func playPauseAction(sender: UIButton) {
+        sender.selected = !sender.selected;
+        if sender.selected {
+            player.play()
+        } else {
+            player.pause()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var error: NSError?
+        var success = AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord, withOptions: .DefaultToSpeaker, error:&error)
+
+        if !success {
+            NSLog("Failed to set audio session category. Error: \(error)")
+        }
+
+        let songNames = ["FeelinGood", "IronBacon", "WhatYouWant"]
+        let songs = songNames.map {
+            AVPlayerItem(URL: NSBundle.mainBundle().URLForResource($0, withExtension: "mp3"))
+        }
+
+        player = AVQueuePlayer(items: songs)
+        player.actionAtItemEnd = .Advance
+
+        player.addObserver(self, forKeyPath: "currentItem", options: .New | .Initial, context: nil)
+
+        player.addPeriodicTimeObserverForInterval(CMTimeMake(1, 100), queue: dispatch_get_main_queue(), usingBlock: {
+            [unowned self] time in
+            let timeString = String(format: "%02.2f", CMTimeGetSeconds(time))
+            if UIApplication.sharedApplication().applicationState == .Active {
+                self.timeLabel.text = timeString
+            } else {
+                println("Background: \(timeString)")
+            }
+        })
+    }
+
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if keyPath == "currentItem",
+        let player = object as? AVPlayer,
+            currentItem = player.currentItem?.asset as? AVURLAsset {
+                songLabel.text = currentItem.URL?.lastPathComponent ?? "Unknown"
+        }
+    }
+}
